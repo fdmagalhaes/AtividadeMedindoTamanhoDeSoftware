16a6a4cfda72016839f845656cb15ca693285f43
fdmagalhaes <83773364+fdmagalhaes@users.noreply.github.com>
2024-03-07 20:19:39 -0300

    Add files via upload

---
+++ Ola/OlaMundo.java
@@ -0,0 +1,9 @@
+public class OlaMundo {
+
+    //Escreve na tela  "Olá Mundo!" usando a função println do Java.
+    public static void main(String[] args) {
+        //O que sera escrito 
+        //o que esta entre ""
+        System.out.println("Olá mundo!");
+    }
+}
